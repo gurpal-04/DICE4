900060a4bfdbe115eaa786d475478d51cddec6ac
gurpal-04 <gurpal1705.be21@chitkara.edu.in>
2023-05-10 11:28:27 +0530

    predict function added

--- main.py
+++ main.py
@@ -9,6 +9,10 @@ app = Flask(__name__)
 def home():
   return render_template("home.html")
 
+@app.route('/predict', methods=['POST'])
+def predict():
+  pred = model.predict(request.form['pic'])
+  return render_template('output.html', data=pred)
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', port=81, debug=True)
